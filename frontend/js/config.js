34fac7ded4441cb55770e80dd8e2c461926d8856
Devika Kundathil Anoop <devika.kundathil086@outlook.com>
2026-04-05 01:20:03 +0530

    Update API_BASE_URL to include '/api' suffix

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
 const API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://127.0.0.1:8000/api"
-    : "https://recipefinder-8ts9.onrender.com/";
+    : "https://recipefinder-8ts9.onrender.com/api";
